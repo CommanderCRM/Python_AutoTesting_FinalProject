f50a1a01bd6a91c5da45a18110a13c4e76d3ef7a
Ilya Krivoshein <htctomsk634@gmail.com>
2022-11-27 00:03:49 +0700

    added registration and tests for registered users

--- pages/locators.py
+++ pages/locators.py
@@ -4,6 +4,7 @@ class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     CART_LINK = (By.CSS_SELECTOR, ".btn-group > a.btn.btn-default")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
     
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
@@ -11,6 +12,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTER_EMAIL_FIELD = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTER_PASSWORD_FIELD = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTER_CONFIRM_PASSWORD_FIELD = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTER_BUTTON = (By.CSS_SELECTOR, "[name='registration_submit']")
 
 class ProductPageLocators():
     ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,8 @@
 import pytest
+import time
 from .pages.product_page import ProductPage
 from .pages.cart_page import CartPage
+from .pages.login_page import LoginPage
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 
@@ -11,17 +13,32 @@ tested_links = [f"{link}?promo=offer{i}" if i not in list_of_failed_num else
                              )
                 for i in range(10)]
 
-@pytest.mark.parametrize("link", tested_links)
-@pytest.mark.skip
-def test_guest_can_add_product_to_cart(browser, link):
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_be_add_to_cart_button()
-    page.add_product_to_cart()
-    page.solve_quiz_and_get_code()
-    page.success_add_message_should_be_present()
-    page.should_be_product_name_in_cart()
-    page.cart_total_should_equal_product_price()
+# @pytest.mark.parametrize("link", tested_links)
+
+class TestUserAddToCartFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+        page = LoginPage(browser, link)
+        page.open()
+        email = str(time.time()) + "@fakemail.org"
+        password = str(time.time())[-9:]
+        page.register_new_user(email, password)
+        page.should_be_authorized_user()
+
+    def test_user_can_add_product_to_cart(self, browser):
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_add_to_cart_button()
+        page.add_product_to_cart()
+        page.success_add_message_should_be_present()
+        page.should_be_product_name_in_cart()
+        page.cart_total_should_equal_product_price()
+
+    def test_user_cant_see_success_message(self, browser):
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
 
 @pytest.mark.xfail
 @pytest.mark.skip
@@ -29,12 +46,6 @@ def test_guest_cant_see_success_message_after_adding_product_to_cart(browser):
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_cart()
-    page.should_not_be_success_message()
-
-@pytest.mark.skip
-def test_guest_cant_see_success_message(browser):
-    page = ProductPage(browser, link)
-    page.open()
     page.should_not_be_success_message()
 
 @pytest.mark.xfail
@@ -59,6 +70,24 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.open()
     page.go_to_login_page()
 
+@pytest.mark.skip
+def test_guest_can_add_product_to_cart(browser, link):
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_add_to_cart_button()
+        page.add_product_to_cart()
+        page.solve_quiz_and_get_code()
+        page.success_add_message_should_be_present()
+        page.should_be_product_name_in_cart()
+        page.cart_total_should_equal_product_price()
+
+@pytest.mark.skip
+def test_guest_cant_see_success_message(self, browser):
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+@pytest.mark.skip
 def test_guest_cant_see_product_in_cart_opened_from_product_page(browser):
     page = ProductPage(browser, prod_page_link)
     page.open()
